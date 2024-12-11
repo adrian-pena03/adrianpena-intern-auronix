a8affdfd31dcd7289085f3f49dc89cea745fbfa4
adrian-pena03 <antares2.pr@gmail.com>
2024-12-10 21:55:30 -0600

    Filtrado agregado para obtener a los personajes que estan vivos.
    Esto se hace utilizando el field 'status' de cada
    personje

--- src/adrianpena.js
+++ src/adrianpena.js
@@ -1,6 +1,8 @@
 // Adrian Pena (adrian.pena@cimat.mx)
 
+
 // Declaramos el link de la API
+// Esta URL se asocia a los personajes de RickAndMorty
 const RickAndMortyAPIURL = "https://rickandmortyapi.com/api/character";
 
 
@@ -13,9 +15,15 @@ async function FiltrarPersonajesVivos() {
             console.log("Error en la GET request");
         }
         
-        // Extraemos los datos originales
+        // Extraemos los datos originales de los personajes
         const data = await response.json();
         console.log(data);
+
+        // Filtramos los personajes vivos, los cuales 
+        // poseen el campo [status: 'Alive']
+        const alive_characters = data.results.filter((character) => character.status === 'Alive');
+        console.log("Personajes vivos: ", alive_characters);
+
 
     } catch(error) {
         console.log("Error en la GET request");
